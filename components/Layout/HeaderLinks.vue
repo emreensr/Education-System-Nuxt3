974a7168056aa3bd20dc4119c28873adbd1b17c3
emreensr <emreensar.capci@gmail.com>
2023-06-08 18:44:58 +0300

    login and register modal added

--- components/Layout/HeaderLinks.vue
+++ components/Layout/HeaderLinks.vue
@@ -1,12 +1,154 @@
+<script setup>
+
+const loginModal = ref(false)
+const registerModal = ref(false)
+
+const openLoginModal = () => {
+  registerModal.value = false;
+  loginModal.value = true;
+};
+
+const openRegisterModal = () => {
+  registerModal.value = true;
+};
+
+const closeModal = () => {
+  loginModal.value = false;
+  registerModal.value = false;
+};
+
+</script>
+
+
 <template>
-  <div>
+<div class="fixed top-0 left-0 w-full h-full bg-black z-50 bg-opacity-10 flex md:items-center justify-center" v-if="loginModal">
+    <div class="bg-white p-6 rounded-lg shadow-lg w-full max-w-2xl overflow-y-auto">
+      <div class="flex items-center justify-between mx-auto mb-6">
+        <h2 class="font-medium text-lg"></h2>
+        <button class="text-gray-600 hover:text-gray-800" @click="closeModal">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400 hover:text-gray-500" fill="none"
+            viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+
+    <div class="flex flex-col justify-center px-6 py-12 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Giriş Yap</h2>
+      </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" action="#" method="POST">
+        <div>
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">E-Posta</label>
+          <div class="mt-2">
+            <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 px-3 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Şifre</label>
+            <div class="text-sm">
+              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Şifremi unuttum?</a>
+            </div>
+          </div>
+          <div class="mt-2">
+            <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 px-3 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Giriş Yap</button>
+        </div>
+      </form>
+
+      <nuxt-link
+      @click="openRegisterModal"
+      >
+      <p class="mt-10 text-center text-sm text-gray-500">
+        Hesabınız yok mu?
+        <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Kaydolun</a>
+      </p>
+    </nuxt-link>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="fixed top-0 left-0 w-full h-full bg-black z-50 bg-opacity-10 flex md:items-center justify-center" v-if="registerModal">
+    <div class="bg-white p-6 rounded-lg shadow-lg w-full max-w-2xl overflow-y-auto">
+      <div class="flex items-center justify-between mx-auto mb-6">
+        <h2 class="font-medium text-lg"></h2>
+        <button class="text-gray-600 hover:text-gray-800" @click="closeModal">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400 hover:text-gray-500" fill="none"
+            viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+
+    <div class="flex flex-col justify-center px-6 py-12 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Kayıt Ol</h2>
+      </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" action="#" method="POST">
+        <div>
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Ad </label>
+          <div class="mt-2">
+            <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 px-3 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          </div>
+        </div>
+        <div>
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Soyad</label>
+          <div class="mt-2">
+            <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 px-3 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          </div>
+        </div>
+        <div>
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">E-Posta</label>
+          <div class="mt-2">
+            <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 px-3 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          </div>
+        </div>
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Şifre</label>
+          </div>
+          <div class="mt-2">
+            <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 px-3 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          </div>
+        </div>
+        <div>
+          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Kayıt Ol</button>
+        </div>
+      </form>
+      <nuxt-link
+      @click="openLoginModal"
+      >
+      <p class="mt-10 text-center text-sm text-gray-500">
+        Hesabınız var mı?
+        <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Giriş yapın</a>
+      </p>
+      </nuxt-link>
+    </div>
+  </div>
+</div>
+</div>
+
+<div>
     <ul
       class="flex items-center space-x-5 absolute right-4 top-1/2 transform -translate-y-1/2 lg:static lg:translate-y-0"
     >
       <li>
         <nuxt-link
-          to="/login"
-          class="flex items-center space-x-2.5 text-sm"
+          @click="openLoginModal"
+          class="flex items-center space-x-2.5 text-sm cursor-pointer"
         >
           <svg xmlns="http://www.w3.org/2000/svg"
           class="w-6 h-6" 
--- components/LoginC.vue
+++ components/LoginC.vue
@@ -1,27 +1,4 @@
 <template>
-    <div>
-        <!--
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
-<!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-white">
-  <body class="h-full">
-  ```
--->
 <div class="flex min-h-full sm:h-screen flex-col justify-center px-6 py-12 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
     <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
@@ -60,6 +37,4 @@
     </p>
   </div>
 </div>
-
-    </div>
 </template>
