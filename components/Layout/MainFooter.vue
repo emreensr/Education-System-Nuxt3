7c50bd9590b068ca88407bbd30da46b1be47d994
MacBook <macbook@Emre-MBP.home>
2023-08-17 14:13:08 +0300

    footer design has changed.

--- components/Layout/MainFooter.vue
+++ components/Layout/MainFooter.vue
@@ -1,162 +1,92 @@
 <template>
-  <footer class="bg-gray-100 border-t space-y-5">
-    <div class="max-w-7xl mx-auto py-10 px-4 lg:px-8 2xl:px-0">
-      <div class="grid grid-cols-1 md:grid-cols-4 gap-y-6 md:gap-y-0 md:gap-12">
-        <div>
-          <div class="text-base mb-4 flex items-center space-x-2">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill="currentColor"
-                  d="M12 14c2.206 0 4-1.794 4-4s-1.794-4-4-4s-4 1.794-4 4s1.794 4 4 4zm0-6c1.103 0 2 .897 2 2s-.897 2-2 2s-2-.897-2-2s.897-2 2-2z"
-                />
-                <path
-                  fill="currentColor"
-                  d="M11.42 21.814a.998.998 0 0 0 1.16 0C12.884 21.599 20.029 16.44 20 10c0-4.411-3.589-8-8-8S4 5.589 4 9.995c-.029 6.445 7.116 11.604 7.42 11.819zM12 4c3.309 0 6 2.691 6 6.005c.021 4.438-4.388 8.423-6 9.73c-1.611-1.308-6.021-5.294-6-9.735c0-3.309 2.691-6 6-6z"
-                />
-              </svg>
-            </div>
-            <span>Konum</span>
+  <div>
+    <footer class="bg-[#12173e] shadow dark:bg-gray-900">
+      <div class="mx-auto max-w-7xl">
+        <div class="grid grid-cols-1 lg:grid-cols-4 lg:gap-10 gap-12 lg:py-12 py-8">
+          <div class="col-span-1 lg:ml-0 ml-3">
+            <img
+              class="w-24 text-white"
+              id="logoMain"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+              alt="Pizzeria Minigolf"
+            />
+            <p class="text-[#b0b0b8] py-5 lg:ml-0 ml-3">
+              Sizi alanında uzman en yakın öğretmene ulaştırarak en verimli şekilde özel ders almanızı sağlar.
+            </p>
           </div>
-          <p class="text-sm mb-4">
-            FaydApp <br /> Tuzla <br /> İstanbul 34900 <br />
-          </p>
-
-          <!-- <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96514.9197173578!2d29.27428972826745!3d40.89181339933851!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cad9a716174373%3A0x1df73419f1930424!2zVHV6bGEvxLBzdGFuYnVsLCBUw7xya2l5ZQ!5e0!3m2!1str!2snl!4v1686229762086!5m2!1str!2snl"
-            class="w-full"
-            frameborder="0"
-            allowfullscreen
-            loading="lazy"
-          ></iframe> -->
+          <div class="col-span-1 lg:ml-0 ml-3">
+            <h1 class="text-white text-xl font-semibold">Kullanıcı İşlemleri</h1>
+            <div class="flex space-x-20">
+              <ul class="pl-4 mt-6 text-[#b0b0b8] space-y-3">
+                <li>Kullanım Şartları</li>
+                <li>Gizlilik Politikamız</li>
+                <li>İade Şartlarımız</li>
+                <li>Mesafeli Satış Sözleşmemiz</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-span-1 lg:ml-0 ml-3">
+            <h1 class="text-white text-xl font-semibold">Özel Ders İşlemleri</h1>
+            <div class="flex space-x-20">
+              <ul class="pl-4 mt-6 text-[#b0b0b8] space-y-3">
+                <li>Özel Ders Talepleri</li>
+                <li>Özel Ders Verme</li>
+                <li>Özel Ders Alma</li>
+                <li>Online Ders</li>
+              </ul>
+            </div>
+          </div>
+           <div class="col-span-1 lg:ml-0 ml-3">
+            <h1 class="text-white text-xl font-semibold">Hesap İşlemleri</h1>
+            <div class="flex space-x-20">
+              <ul class="pl-4 mt-6 text-[#b0b0b8] space-y-3">
+                <li>Giriş Yap</li>
+                <li>Kayıt Ol</li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div>
-          <div class="text-base mb-4 flex items-center space-x-2">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill="currentColor"
-                  d="M12 20c4.4 0 8-3.6 8-8s-3.6-8-8-8s-8 3.6-8 8s3.6 8 8 8m0-18c5.5 0 10 4.5 10 10s-4.5 10-10 10S2 17.5 2 12S6.5 2 12 2m5 9.5V13h-6V7h1.5v4.5H17Z"
-                />
-              </svg>
-            </div>
-            <span>Kullanıcı İşlemleri</span>
+        <hr class="border border-solid border-gray-800" />
+        <div class="lg:flex justify-between items-center pt-5 pb-8">
+          <div class="text-[#b0b0b8] lg:ml-0 ml-3">
+            <h1>
+              © 2023 <span class="text-white">Egitiva</span> Tüm hakları saklıdır.
+            </h1>
           </div>
-          <div class="space-y-4 text-sm">
-            <ul class="space-y-2">
-              <li>Kullanım Şartları</li>
-              <li>Gizlilik Politikamız</li>
-              <li>İade Şartlarımız</li>
-              <li>Mesafeli Satış Sözleşmemiz</li>
+          <div>
+            <ul class="flex text-white gap-2 lg:ml-0 ml-3 lg:mt-0 mt-3">
+              <li>
+                <div
+                  class="flex lg:col-span-2 h-10 w-10 rounded-md bg-[#ffffff1a] items-center justify-center"
+                >
+                  <Icon name="gg:facebook" size="20" />
+                </div>
+              </li>
+              <li>
+                <div
+                  class="flex lg:col-span-2 h-10 w-10 rounded-md bg-[#ffffff1a] items-center justify-center"
+                >
+                  <Icon name="jam:linkedin" size="20" />
+                </div>
+              </li>
+              <li>
+                <div
+                  class="flex lg:col-span-2 h-10 w-10 rounded-md bg-[#ffffff1a] items-center justify-center"
+                >
+                  <Icon name="mdi:twitter" size="20" />
+                </div>
+              </li>
+              <li>
+                <div
+                  class="flex lg:col-span-2 h-10 w-10 rounded-md bg-[#ffffff1a] items-center justify-center"
+                >
+                  <Icon name="mdi:youtube" size="20" />
+                </div>
+              </li>
             </ul>
           </div>
         </div>
-        <div>
-          <div class="text-base mb-4 flex items-center space-x-2">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill="currentColor"
-                  d="M17.707 12.293a.999.999 0 0 0-1.414 0l-1.594 1.594c-.739-.22-2.118-.72-2.992-1.594s-1.374-2.253-1.594-2.992l1.594-1.594a.999.999 0 0 0 0-1.414l-4-4a.999.999 0 0 0-1.414 0L3.581 5.005c-.38.38-.594.902-.586 1.435c.023 1.424.4 6.37 4.298 10.268s8.844 4.274 10.269 4.298h.028c.528 0 1.027-.208 1.405-.586l2.712-2.712a.999.999 0 0 0 0-1.414l-4-4.001zm-.127 6.712c-1.248-.021-5.518-.356-8.873-3.712c-3.366-3.366-3.692-7.651-3.712-8.874L7 4.414L9.586 7L8.293 8.293a1 1 0 0 0-.272.912c.024.115.611 2.842 2.271 4.502s4.387 2.247 4.502 2.271a.991.991 0 0 0 .912-.271L17 14.414L19.586 17l-2.006 2.005z"
-                />
-              </svg>
-            </div>
-            <span>Özel Ders İşlemleri</span>
-          </div>
-          <ul class="space-y-2 text-sm">
-            <li>Özel Ders Talepleri</li>
-            <li>Özel Ders Verme</li>
-            <li>Özel Ders Alma</li>
-            <li>Online Ders</li>
-
-          </ul>
-          <!-- <div class="pt-4 text-sm">
-            <nuxt-link
-              to="mailto:pizzeria@ristorante-minigolf.de"
-              class="text-accent hover:brightness-75"
-            >
-              pizzeria@ristorante-minigolf.de
-            </nuxt-link>
-          </div> -->
-        </div>
-        <div>
-          <div class="text-base mb-4 flex items-center space-x-2">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill="currentColor"
-                  d="M21 8c-.202 0-4.85.029-9 2.008C7.85 8.029 3.202 8 3 8a1 1 0 0 0-1 1v9.883a1 1 0 0 0 .305.719c.195.188.48.305.729.28l.127-.001c.683 0 4.296.098 8.416 2.025c.016.008.034.005.05.011c.119.049.244.083.373.083s.254-.034.374-.083c.016-.006.034-.003.05-.011c4.12-1.928 7.733-2.025 8.416-2.025l.127.001c.238.025.533-.092.729-.28c.194-.189.304-.449.304-.719V9a1 1 0 0 0-1-1zM4 10.049c1.485.111 4.381.48 7 1.692v7.742c-3-1.175-5.59-1.494-7-1.576v-7.858zm16 7.858c-1.41.082-4 .401-7 1.576v-7.742c2.619-1.212 5.515-1.581 7-1.692v7.858z"
-                />
-                <circle cx="12" cy="5" r="3" fill="currentColor" />
-              </svg>
-            </div>
-            <span>Hesap İşlemleri</span>
-          </div>
-          <ul class="space-y-2 text-sm">
-            <li>Giriş Yap</li>
-            <li>Kayıt Ol</li>
-          </ul>
-        </div>
       </div>
-    </div>
-    <div class="bg-black py-5">
-      <div class="max-w-7xl mx-auto">
-        <!-- <p class="text-white text-center mb-4 text-sm">sponsored by</p> -->
-        <ul class="flex items-center space-x-4 justify-center pt-6">
-          <li>
-            <nuxt-link to="/" title="Instagram Profile">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                data-name="Layer 1"
-                viewBox="0 0 24 24"
-                class="w-7 h-7 text-white hover:brightness-125"
-                fill="currentColor"
-              >
-                <path
-                  d="M17.34,5.46h0a1.2,1.2,0,1,0,1.2,1.2A1.2,1.2,0,0,0,17.34,5.46Zm4.6,2.42a7.59,7.59,0,0,0-.46-2.43,4.94,4.94,0,0,0-1.16-1.77,4.7,4.7,0,0,0-1.77-1.15,7.3,7.3,0,0,0-2.43-.47C15.06,2,14.72,2,12,2s-3.06,0-4.12.06a7.3,7.3,0,0,0-2.43.47A4.78,4.78,0,0,0,3.68,3.68,4.7,4.7,0,0,0,2.53,5.45a7.3,7.3,0,0,0-.47,2.43C2,8.94,2,9.28,2,12s0,3.06.06,4.12a7.3,7.3,0,0,0,.47,2.43,4.7,4.7,0,0,0,1.15,1.77,4.78,4.78,0,0,0,1.77,1.15,7.3,7.3,0,0,0,2.43.47C8.94,22,9.28,22,12,22s3.06,0,4.12-.06a7.3,7.3,0,0,0,2.43-.47,4.7,4.7,0,0,0,1.77-1.15,4.85,4.85,0,0,0,1.16-1.77,7.59,7.59,0,0,0,.46-2.43c0-1.06.06-1.4.06-4.12S22,8.94,21.94,7.88ZM20.14,16a5.61,5.61,0,0,1-.34,1.86,3.06,3.06,0,0,1-.75,1.15,3.19,3.19,0,0,1-1.15.75,5.61,5.61,0,0,1-1.86.34c-1,.05-1.37.06-4,.06s-3,0-4-.06A5.73,5.73,0,0,1,6.1,19.8,3.27,3.27,0,0,1,5,19.05a3,3,0,0,1-.74-1.15A5.54,5.54,0,0,1,3.86,16c0-1-.06-1.37-.06-4s0-3,.06-4A5.54,5.54,0,0,1,4.21,6.1,3,3,0,0,1,5,5,3.14,3.14,0,0,1,6.1,4.2,5.73,5.73,0,0,1,8,3.86c1,0,1.37-.06,4-.06s3,0,4,.06a5.61,5.61,0,0,1,1.86.34A3.06,3.06,0,0,1,19.05,5,3.06,3.06,0,0,1,19.8,6.1,5.61,5.61,0,0,1,20.14,8c.05,1,.06,1.37.06,4S20.19,15,20.14,16ZM12,6.87A5.13,5.13,0,1,0,17.14,12,5.12,5.12,0,0,0,12,6.87Zm0,8.46A3.33,3.33,0,1,1,15.33,12,3.33,3.33,0,0,1,12,15.33Z"
-                />
-              </svg>
-            </nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/" title="Facebook Profile">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="w-5 h-5 text-white hover:brightness-125"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-              >
-                <path
-                  d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
-                />
-              </svg>
-            </nuxt-link>
-          </li>
-        </ul>
-        <p class="text-white text-sm text-center mt-4">
-          © 2023 faydApp.com - Tüm hakları saklıdır.
-        </p>
-      </div>
-    </div>
-  </footer>
+    </footer>
+  </div>
 </template>
