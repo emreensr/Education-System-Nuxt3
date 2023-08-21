7c34292a669a61fde7dbeec145c4bf59b33221af
MacBook <macbook@Emre-MBP.home>
2023-08-22 00:40:47 +0300

    fix category section issue

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@ definePageMeta({
 
 <template>
     <HomeMainSlider />
-    <HomeCategorySection />
+    <HomeCategorySection /> 
     <h1 class="text-center font-semibold text-2xl lg:text-3xl mb-3 mt-8">EN İYİ ÖĞRETMENLER</h1>
     <p class="text-center">Kategorisinden en iyi öğretmenleri sizin için listeledik.</p>
      <div class="mx-auto max-w-6xl grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8 mt-6 p-5">
