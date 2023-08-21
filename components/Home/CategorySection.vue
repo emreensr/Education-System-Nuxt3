e5f148d6841457fa3b174d465cea7542276d2158
MacBook <macbook@Emre-MBP.home>
2023-08-21 22:02:38 +0300

    added category section

---
+++ components/Home/CategorySection.vue
@@ -0,0 +1,137 @@
+
+<template>
+    <div class="bg-[#f8f8f8]">
+        <div data-aos="fade-up" data-aos-once="true" data-aos-delay="50" data-aos-offset="200" data-aos-duration="800"
+            class="mt-8">  
+            <div class="text-center lg:pt-24 pt-8 p-3 space-y-3 lg:pb-12">
+                <h1 class="font-semibold text-xl text-blue-700">KATEGORİLER</h1>
+                <h1 class="text-3xl font-semibold">Kategorilerde Birleşen Bilgi Dünyası</h1>
+                <p class="text-[#808080]">İhtiyacınıza uygun kategoriyi seçin, eğitmenlerimizin sunduğu zengin içeriklerle
+                    bilgi
+                    dolu bir yolculuğa çıkın!</p>
+            </div>
+            <div class="mx-auto max-w-6xl grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-3 py-8 lg:pb-24">
+                <div class="bg-white flex md:flex-col md:py-12 md:space-y-5 text-center shadow-xs p-4 border-b transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-md cursor-pointer">
+                    <div
+                    class="flex bg-red-400 h-12 w-12 md:h-[60px] md:w-[60px] rounded-full justify-center items-center md:mx-auto ml">
+                        <img src="/matematik.png" />
+                    </div>
+                    <div class="flex-grow ml-4 md:ml-0 flex flex-col justify-center">
+                        <div class="flex md:flex-col justify-between items-center md:space-y-5">
+                            <h1 class="font-normal text-lg hover:text-[#010C20]">Matematik</h1>
+                            <p class="text-[14px] text-[#808080]">127 Öğretmen</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white flex md:flex-col md:py-12 md:space-y-5 text-center shadow-xs p-4 border-b transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-md cursor-pointer">
+                    <div
+                        class="flex bg-blue-400 h-12 w-12 md:h-[60px] md:w-[60px] rounded-full justify-center items-center md:mx-auto ml">
+                        <img src="/fizik.png" />
+                    </div>
+                    <div class="flex-grow ml-4 md:ml-0 flex flex-col justify-center">
+                        <div class="flex md:flex-col justify-between items-center md:space-y-5">
+                            <h1 class="font-normal text-lg hover:text-[#010C20]">Fizik</h1>
+                            <p class="text-[14px] text-[#808080]">110 Öğretmen</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white flex md:flex-col md:py-12 md:space-y-5 text-center shadow-xs p-4 border-b transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-md cursor-pointer">
+                    <div
+                    class="flex bg-pink-400 h-12 w-12 md:h-[60px] md:w-[60px] rounded-full justify-center items-center mx-auto">
+                    <img src="/ingilizce.png" />
+                    </div>
+                    <div class="flex-grow ml-4 md:ml-0 flex flex-col justify-center">
+                        <div class="flex md:flex-col justify-between items-center md:space-y-5">
+                            <h1 class="font-normal text-lg hover:text-[#010C20]">İngilizce</h1>
+                            <p class="text-[14px] text-[#808080]">85 Öğretmen</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white flex md:flex-col md:py-12 md:space-y-5 text-center shadow-xs p-4 border-b transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-md cursor-pointer">
+                    <div
+                    class="flex bg-green-400 h-12 w-12 md:h-[60px] md:w-[60px] rounded-full justify-center items-center mx-auto">
+                    <img src="/geometri.png" />
+                    </div>
+                    <div class="flex-grow ml-4 md:ml-0 flex flex-col justify-center">
+                        <div class="flex md:flex-col justify-between items-center md:space-y-5">
+                            <h1 class="font-normal text-lg hover:text-[#010C20]">Geometri</h1>
+                            <p class="text-[14px] text-[#808080]">135 Öğretmen</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white flex md:flex-col md:py-12 md:space-y-5 text-center shadow-xs p-4 border-b transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-md cursor-pointer">
+                    <div
+                    class="flex bg-yellow-300 h-12 w-12 md:h-[60px] md:w-[60px] rounded-full justify-center items-center mx-auto">
+                    <img src="/tarih.png" />
+                    </div>
+                    <div class="flex-grow ml-4 md:ml-0 flex flex-col justify-center">
+                        <div class="flex md:flex-col justify-between items-center md:space-y-5">
+                            <h1 class="font-normal text-lg hover:text-[#010C20]">Tarih</h1>
+                            <p class="text-[14px] text-[#808080]">35 Öğretmen</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white flex md:flex-col md:py-12 md:space-y-5 text-center shadow-xs p-4 border-b transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-md cursor-pointer">
+                    <div
+                    class="flex bg-slate-400 h-12 w-12 md:h-[60px] md:w-[60px] rounded-full justify-center items-center mx-auto">
+                    <img src="/resim.png" />
+                    </div>
+                    <div class="flex-grow ml-4 md:ml-0 flex flex-col justify-center">
+                        <div class="flex md:flex-col justify-between items-center md:space-y-5">
+                            <h1 class="font-normal text-lg hover:text-[#010C20]">Resim</h1>
+                            <p class="text-[14px] text-[#808080]">15 Öğretmen</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white flex md:flex-col md:py-12 md:space-y-5 text-center shadow-xs p-4 border-b transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-md cursor-pointer">
+                    <div
+                    class="flex bg-indigo-400 h-12 w-12 md:h-[60px] md:w-[60px] rounded-full justify-center items-center mx-auto">
+                    <img src="/yazilim.png" />
+                    </div>
+                    <div class="flex-grow ml-4 md:ml-0 flex flex-col justify-center">
+                        <div class="flex md:flex-col justify-between items-center md:space-y-5">
+                            <h1 class="font-normal text-lg hover:text-[#010C20]">Yazılım</h1>
+                            <p class="text-[14px] text-[#808080]">45 Öğretmen</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white flex md:flex-col md:py-12 md:space-y-5 text-center shadow-xs p-4 border-b transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-md cursor-pointer">
+                    <div
+                    class="flex bg-orange-300 h-12 w-12 md:h-[60px] md:w-[60px] rounded-full justify-center items-center mx-auto">
+                    <img src="/kimya.png" />
+                    </div>
+                    <div class="flex-grow ml-4 md:ml-0 flex flex-col justify-center">
+                        <div class="flex md:flex-col justify-between items-center md:space-y-5">
+                            <h1 class="font-normal text-lg hover:text-[#010C20]">Kimya</h1>
+                            <p class="text-[14px] text-[#808080]">55 Öğretmen</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white flex md:flex-col md:py-12 md:space-y-5 text-center shadow-xs p-4 border-b transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-md cursor-pointer">
+                    <div
+                    class="flex bg-teal-400 h-12 w-12 md:h-[60px] md:w-[60px] rounded-full justify-center items-center mx-auto">
+                    <img src="/universite-hazirlik.png" />
+                    </div>
+                    <div class="flex-grow ml-4 md:ml-0 flex flex-col justify-center">
+                        <div class="flex md:flex-col justify-between items-center md:space-y-5">
+                            <h1 class="font-normal text-lg hover:text-[#010C20]">Üniversite Takviye</h1>
+                            <p class="text-[14px] text-[#808080]">150 Öğretmen</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white flex md:flex-col md:py-12 md:space-y-5 text-center shadow-xs p-4 border-b transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-md cursor-pointer">
+                    <div
+                    class="flex bg-lime-400 h-12 w-12 md:h-[60px] md:w-[60px] rounded-full justify-center items-center mx-auto">
+                    <img src="/muzik.png" />
+                    </div>
+                    <div class="flex-grow ml-4 md:ml-0 flex flex-col justify-center">
+                        <div class="flex md:flex-col justify-between items-center md:space-y-5">
+                            <h1 class="font-normal text-lg hover:text-[#010C20]">Müzik</h1>
+                            <p class="text-[14px] text-[#808080]">95 Öğretmen</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
